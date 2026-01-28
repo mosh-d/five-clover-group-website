00a2e6deb42af67be1631cfab604353fc7599b7c
mosh-d <shopsy8@gmail.com>
2026-01-28 16:30:55 +0100

    Finishing Hero section

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,12 +1,67 @@
 import FiveClover from "@/assets/home/hero/five-clover.jpg";
 
+import FiveCloverLogo from "@/assets/home/hero/logos/five-clover-logo.png";
+import CaritasLogo from "@/assets/home/hero/logos/caritas-logo.png";
+import RingRubyLogo from "@/assets/home/hero/logos/ring-ruby-logo.png";
+import CordisLogo from "@/assets/home/hero/logos/cordis-logo.png";
+
+const LOGOS = [
+    {
+        name: "Five Clover",
+        logo: FiveCloverLogo,
+    },
+    {
+        name: "Caritas",
+        logo: CaritasLogo,
+    },
+    {
+        name: "RingRuby",
+        logo: RingRubyLogo,
+    },
+    {
+        name: "Cordis",
+        logo: CordisLogo,
+    },
+]
+
 export default function Hero() {
   return (
     <div
-      className="bg-cover bg-center h-[100vh] w-full"
-      style={{ backgroundImage: `url(${FiveClover.src})` }}
+      className="bg-cover bg-center h-[100vh] min-h-[80rem] w-full flex flex-col justify-center items-center"
+      style={{
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, .95), rgba(0, 0, 0, .6)), url(${FiveClover.src})`,
+      }}
     >
-      <h1>Hero</h1>
+      <div className="flex flex-col gap-[1rem] px-[12rem] py-[18rem] h-full items-center justify-around">
+        <div className="flex flex-col gap-[2.4rem] items-center justify-center">
+          <h1 className="text-[var(--white)] font-secondary text-[3.4rem] text-center">
+            Experience The Art of Hospitality—Multiplied
+          </h1>
+          <p className="font-secondary text-[var(--white)] text-[1.6rem] text-center">
+            Welcome to Five Clover Hotels, the premier hotel management company
+            behind three distinct brands: Five Clover Hotels, Caritas Inns, and
+            RingRuby Hotels. Together, our 2 Five Clover properties, 4 Caritas
+            locations, and 3 RingRuby curated experiences across Lagos deliver a
+            unified promise of elegance, comfort, and personalized service—each
+            with its own unique character.
+          </p>
+        </div>
+        <div>
+          <h2 className="text-[var(--white)] font-secondary text-[5.3rem] font-medium text-center">
+            Our Brands
+          </h2>
+          <div className="flex gap-[4.8rem] items-center justify-center">
+            {LOGOS.map((logo) => (
+              <div key={logo.name} className="flex flex-col gap-[.1rem] items-center justify-center">
+                <img src={logo.logo.src} alt={logo.name} className="size-[10rem] object-contain" />
+                <h3 className="text-[var(--white)] text-[1.4rem] font-medium text-center">
+                  {logo.name}
+                </h3>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/shared/TopBar.jsx
+++ src/components/shared/TopBar.jsx
@@ -12,43 +12,48 @@ export default function TopBar() {
 
   return (
     <div className="absolute top-0 w-full flex justify-between items-center h-[10rem] py-[1.2rem] px-[9rem] z-10">
-      <ul className="flex gap-[3.2rem]">
-        <li>
-          <NavLink href="/" className={isHomePage ? "text-[var(--white)]" : ""}>
-            HOME
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            href="/about"
-            className={isHomePage ? "text-[var(--white)]" : ""}
-          >
-            ABOUT
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            href="/contact"
-            className={isHomePage ? "text-[var(--white)]" : ""}
-          >
-            CONTACT
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            href="/blog"
-            className={isHomePage ? "text-[var(--white)]" : ""}
-          >
-            BLOG
-          </NavLink>
-        </li>
-      </ul>
-      <div className="size-[10rem] absolute top-[1.2rem] left-1/2 translate-x-[-50%]">
-        <Image src={Logo} alt="Five Clover Logo" />
+      <div className="border-b-[1px] w-full h-[12rem] border-[var(--emphasis)]/50 flex justify-between items-center">
+        <ul className="flex gap-[3.2rem]">
+          <li>
+            <NavLink
+              href="/"
+              className={isHomePage ? "text-[var(--white)]" : ""}
+            >
+              HOME
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              href="/about"
+              className={isHomePage ? "text-[var(--white)]" : ""}
+            >
+              ABOUT
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              href="/contact"
+              className={isHomePage ? "text-[var(--white)]" : ""}
+            >
+              CONTACT
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              href="/blog"
+              className={isHomePage ? "text-[var(--white)]" : ""}
+            >
+              BLOG
+            </NavLink>
+          </li>
+        </ul>
+        <div className="size-[10rem] absolute top-[1.2rem] left-1/2 translate-x-[-50%]">
+          <Image src={Logo} alt="Five Clover Logo" />
+        </div>
+        <Button href="/">
+          <p className="text-[1.8rem] text-[var(--emphasis)]">RESERVE</p>
+        </Button>
       </div>
-      <Button href="/">
-        <p className="text-[1.8rem] text-[var(--emphasis)]">RESERVE</p>
-      </Button>
     </div>
   );
 }
